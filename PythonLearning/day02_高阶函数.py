fa2215b45d81ad7f127a7c2084d3da7998240777
wangjianbing1998 <2553627958@qq.com>
2021-10-17 21:59:27 +0800

    第二天的高阶函数的作业学习

--- PythonLearning/day02_高阶函数.py
+++ PythonLearning/day02_高阶函数.py
@@ -1,11 +1,11 @@
 from functools import reduce
 from typing import List
-from time import time
+import time
 import functools
 
 
 class Item:
-    def __init__(self, name:str, count:str, id:int):
+    def __init__(self, name: str, count: str, id: int):
         self.name = name
         self.count = count
         self.id = id
@@ -20,12 +20,16 @@ def filterMapReduce(items: List[Item]) -> int:
     2. 将所有符合条件的Item的count转化为int
     3. 返回所有符合条件的Item的count之和
     要求需要使用filter和map、reduce高级的方法
+    TODO sum在这里不可以替代lambda表达式 lambda x,y:x+y
+    :return:
     >>> itemList=[Item(str(i+10),str(i+100),i) for i in range(5)]
     >>> print(filterMapReduce(itemList))
     207
-    :return:
     """
-    return reduce(lambda x, y: x + y, map(lambda x: int(x.count), filter(lambda x: x.id > 2, items)))
+    # return reduce(lambda x, y: x + y, map(lambda x: int(x.count), filter(lambda x: x.id > 2, items)))
+    m = map(lambda x: int(x.count), filter(lambda x: x.id > 2, items))
+    # return reduce(sum, m, 0)
+    return reduce(lambda x,y:x+y, m, 0)
 
 
 def sortedFunction(items: List[Item]) -> List[int]:
@@ -43,19 +47,28 @@ def decoratorDemo(func):
     """
     实现自动给一个函数统计执行时间的方法
     :return:
+    wrapper=decoratorDemo(testDecoratorDemo)
+    func_resule=wrapper(*args,**kw)
     """
+
     def wrapper(*args, **kw):
-        start = time()
+        start = time.time()
         func_return = func(*args, **kw)
-        end = time()
-        print(f"{func.__name__}() run time: {end - start}s")
+        print(f"{func.__name__}() run time: {time.time() - start}s")
         return func_return
+
     return wrapper
+
 
 @decoratorDemo
 def testDecoratorDemo():
+    """
+    :return:
+    """
     print(f"testDecoratorDemo runing")
+    time.sleep(3)
 
+testDecoratorDemo()
 
 
 
@@ -63,11 +76,12 @@ def testPartitionDemo(f):
     """
     给一个两个参数的函数
     返回一个偏函数，其中第一个参数设置为10
+    :return:
     >>> f=lambda x,y:x+y
     >>> f10=testPartitionDemo(f)
     >>> f10(2)
     12
-    :return:
     """
-    return functools.partial(f,10)
+    return functools.partial(f, 10)
+
 
